bbfa22745653031650eaf7b9c1283d273ac98d83
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-05 16:31:05 +0000

    add User table

--- src/models.py
+++ src/models.py
@@ -1,6 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String
+db = SQLAlchemy()
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__= 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(1600), unique=False, nullable=False)
+    
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "email": self.email,
+            "password": self.password,
+            
+        }
 
 class Projects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +30,8 @@ class Projects(db.Model):
     image = db.Column(db.String(120), unique=False, nullable=False)
     github = db.Column(db.String(120), unique=True, nullable=True)
     demo = db.Column(db.String(120), unique=True, nullable=True)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Project %r>' % self.name
@@ -27,6 +50,8 @@ class TechSkills(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     skillImage = db.Column(db.String(60), unique=False, nullable=False)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return '<TechSkills %r' % self.name
@@ -42,6 +67,8 @@ class SoftSkills(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     skillImage = db.Column(db.String(60), unique=False, nullable=False)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return '<TechSkills %r' % self.name
@@ -57,6 +84,8 @@ class Articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=False, nullable=False)
     description = db.Column(db.String(1000), unique=False, nullable=False)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Articles %r' % self.title
@@ -73,6 +102,8 @@ class About(db.Model):
     description = db.Column(db.String(1600), unique=False, nullable=False)
     image = db.Column(db.String(60), unique=True, nullable=False)
     resume = db.Column(db.String(60), unique=True, nullable=False)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return '<About %r' % self.decription
