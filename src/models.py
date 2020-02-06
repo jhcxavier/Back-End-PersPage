0650aabc6543a779a88d26d8f3601c521798f383
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-06 20:09:51 +0000

    updating table User

--- src/models.py
+++ src/models.py
@@ -8,8 +8,9 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__= 'user'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(1600), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
     
 
     def __repr__(self):
@@ -18,6 +19,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id, 
+            "name":self.name,
             "email": self.email,
             "password": self.password,
             
